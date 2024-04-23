f357bb2e0f6909fc259b695588c6a17403f0634e
bernadinedao <bernadine.dao@gmail.com>
2024-04-23 21:10:21 +1000

    updated

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     console.log("Hello");
     // Globals
+
+    // States 
+    let twoPlayer = 0;
+    let botSmart = 1;
+    let botDumb = 2;
+    let mode = twoPlayer;
+
+    let userTurn = 3;
+    let opponentTurn = 4;
+    let whoseTurn = userTurn;
+
     // Cache DOM element references
     // Setup event listeners
     // Event handlers
@@ -11,3 +21,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 });
+
