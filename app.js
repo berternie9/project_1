732ab06573b469ea106d9a9ce359148374e2d8c5
bernadinedao <bernadine.dao@gmail.com>
2024-04-23 15:45:29 +1000

    added favicon

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-console.log("Hello");
-// Globals
-// Cache DOM element references
-// Setup event listeners
-// Event handlers
-// Other functions 
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Hello");
+    // Globals
+    // Cache DOM element references
+    // Setup event listeners
+    // Event handlers
+    // Other functions 
+
+
+
+
+});
